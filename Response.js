9982cc79b436b1c1cb9b384a32ca60ce162d3f1a
briiiqtt <briiiqtt@gmail.com>
2022-05-13 17:48:52 +0900

    d

--- Response.js
+++ Response.js
@@ -15,35 +15,35 @@ const Response = class {
     this.res.send(resp);
   }
   OK(msg) {
-    this.res.status(200);
+    // this.res.status(200);
     this.code = 0;
     this.message = _NAMESPACE.RES_MSG.OK;
     this.message += msg === undefined ? "" : `: ${msg}`;
     this.sendResponse();
   }
   badRequest(msg) {
-    this.res.status(400);
+    // this.res.status(400);
     this.code = 1;
     this.message = _NAMESPACE.RES_MSG.BAD_REQUEST;
     this.message += msg === undefined ? "" : `: ${msg}`;
     this.sendResponse();
   }
   notFound(msg) {
-    this.res.status(404);
+    // this.res.status(404);
     this.code = 2;
     this.message = _NAMESPACE.RES_MSG.NOT_FOUND;
     this.message += msg === undefined ? "" : `: ${msg}`;
     this.sendResponse();
   }
   methodNotAllowed(msg) {
-    this.res.status(405);
+    // this.res.status(405);
     this.code = 3;
     this.message = _NAMESPACE.RES_MSG.METHOD_NOT_ALLOWED;
     this.message += msg === undefined ? "" : `: ${msg}`;
     this.sendResponse();
   }
   internalServerError(msg) {
-    this.res.status(500);
+    // this.res.status(500);
     this.code = 11;
     this.message = _NAMESPACE.RES_MSG.INTERNAL_SERVER_ERROR;
     this.message += msg === undefined ? "" : `: ${msg}`;
